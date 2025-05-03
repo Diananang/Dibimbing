d3d4e81fd8f32449e4715bf57b65fa8aba0ab515
Try Diana Yunita <111040537+Diananang@users.noreply.github.com>
2025-05-03 22:43:21 +0700

    Penyempurnaan Soal 2.js

--- Live Coding/Live Coding 4/Soal 2.js
+++ Live Coding/Live Coding 4/Soal 2.js
@@ -3,23 +3,24 @@ let daftarHarga = [120000, 40000, 75000, 50000, 30000];
 //hitung harga setelah diskon 10%
 //Mengecek apakah produk termasuk premium (harga setelah diskon > 50.000)
 class Harga {
+    constructor (harga){
+        this.harga = harga;
+    }
     arrayHarga = [];
     premiumList = [];
-    afterDiscount(arr){
-        // return (this.arrayHarga = arr.map(item => item - item*0.1));
-        arr.forEach(element => {
-            let hargaDiskon = element - element*0.1;
-            this.arrayHarga.push(hargaDiskon);
-        });
+    afterDiscount(){
+        this.arrayHarga = this.harga.map(element => element - element*0.1, 0);
         console.log("Harga setelah diskon:" + this.arrayHarga);
     }
 
     premium(){
-        return (this.premiumList= this.afterDiscount.reduce((acc, curr) => acc(curr > 50000)));
+        let premiumList = this.arrayHarga.filter(element => element > 50000, 0);
+        console.log("Harga dengan produk premium: " + premiumList);
+        
     }
 }
 
-const output = new Harga ();
+const output = new Harga (daftarHarga);
 
-output.afterDiscount(daftarHarga);
-output.premium(daftarHarga);
+output.afterDiscount();
+output.premium();
